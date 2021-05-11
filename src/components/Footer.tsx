0e40c6ed6132489e3fba30cbd31587b40d39f481
Juan Silveira <juansilveira@gmail.com>
2021-05-11 20:08:43 -0300

    footer message

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
     <footer className="footer-container">
       <p>
         LP-CALC by <b><a href="https://github.com/juan-silveira/lp-calc" target="_blank">juan-silveira.</a></b> The source code is
-        licensed <b>MIT</b>. 💚 Special thanks to all contributers.
+        licensed <b>MIT</b>. Make a fork and PR any missing farm.
       </p>
     </footer>
   );
